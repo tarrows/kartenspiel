7d0ab4b722ed9ef41c352cf7b884b91daa47d5bd
tarrows <c10t@fa.ke>
2020-01-04 23:21:57 +0900

    add styled-components

---
+++ src/components/ad-atoms/Card.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import styled, { css } from 'styled-components'
+
+const Card = styled.div`
+  border: 1px solid #000;
+  margin: 4px;
+  width: 300px;
+  height: 400px;
+`
+
+export default Card
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.scss'
+import Card from './components/ad-atoms/Card'
 
-ReactDOM.render(<h1>OK</h1>, document.getElementById('root'))
+ReactDOM.render(<Card><h1>OK</h1></Card>, document.getElementById('root'))
